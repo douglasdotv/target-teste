696c7e03f21e26f6e62137cba70e6f927bdcfa9a
Douglas Vieira <douglasdotv@hotmail.com>
2023-02-27 15:18:59 -0300

    Add Question2

--- src/main/java/br/com/dv/Main.java
+++ src/main/java/br/com/dv/Main.java
@@ -1,10 +1,17 @@
 package br.com.dv;
 
 public class Main {
+
     public static void main(String[] args) {
         // Question 1
         System.out.println("Questão 1: ");
         Question1.doQuestion1();
         System.out.println();
+
+        // Question 2
+        System.out.println("Questão 2: ");
+        Question2.doQuestion2();
+        System.out.println();
     }
+
 }
--- src/main/java/br/com/dv/Question2.java
+++ src/main/java/br/com/dv/Question2.java
@@ -1,18 +1,49 @@
 package br.com.dv;
 
-public class Question1 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-    public static void doQuestion1() {
-        int index = 13;
-        int sum = 0;
-        int k = 0;
+public class Question2 {
+    /*
+    Escreva um programa que, informado um número, calcule a sequência de Fibonacci e
+    retorne uma mensagem avisando se o número informado pertence ou não à sequência.
+     */
 
-        while (k < index) {
-            k++;
-            sum += k;
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void doQuestion2() {
+        System.out.println("Digite um número: ");
+        int number = scanner.nextInt();
+
+        List<Integer> fibonacciSequence = getFibonacciSequence(number);
+
+        if (fibonacciSequence.contains(number)) {
+            System.out.println("O número " + number + " é um número de Fibonacci!");
+            System.out.println("E a sequência de Fibonacci até o número " + number + " é: " + fibonacciSequence);
+        } else {
+            fibonacciSequence.remove(fibonacciSequence.size() - 1);
+            System.out.println("O número " + number + " não é um número de Fibonacci…");
+            System.out.println("E a sequência de Fibonacci até o número " + number + " é: " + fibonacciSequence);
+        }
+    }
+
+    private static List<Integer> getFibonacciSequence(int number) {
+        int first = 0;
+        int second = 1;
+
+        List<Integer> fibonacciSequence = new ArrayList<>();
+        fibonacciSequence.add(first);
+        fibonacciSequence.add(second);
+
+        while (second <= number) {
+            int sum = first + second;
+            fibonacciSequence.add(sum);
+            first = second;
+            second = sum;
         }
 
-        System.out.println("Soma: " + sum);
+        return fibonacciSequence;
     }
 
 }
